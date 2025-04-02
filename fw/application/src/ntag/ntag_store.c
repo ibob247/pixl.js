45eb63fbe8911adb02dbabe67f06c6bb284764fd
ibob247 <30962100+ibob247@users.noreply.github.com>
2025-04-02 18:26:44 -0400

    Update ntag_store.c

--- fw/application/src/ntag/ntag_store.c
+++ fw/application/src/ntag/ntag_store.c
@@ -4,7 +4,6 @@
 
 #include "ntag_store.h"
 #include "ntag_emu.h"
-#include "ff.h"
 #include "nrf_log.h"
 #include <string.h>
 
@@ -12,27 +11,28 @@ static ntag_update_cb_t emu_callback = NULL;
 static void* emu_context = NULL;
 static ntag_t current_tag;
 
-bool ntag_store_load_from_dump(const char* path) {
-    FIL file;
-    UINT bytes_read;
-
-    if (f_open(&file, path, FA_READ) != FR_OK) return false;
-
-    f_read(&file, current_tag.data, sizeof(current_tag.data), &bytes_read);
-    f_close(&file);
-
-    if (bytes_read != sizeof(current_tag.data)) return false;
-
-    if (emu_callback) emu_callback(NTAG_EVENT_TYPE_UPDATE, emu_context);
-
-    if (emu_callback) {
-        uint8_t* uid = &current_tag.data[0];
-        NRF_LOG_INFO("Preserving UID from dump: %02X %02X %02X %02X %02X %02X %02X",
-                     uid[0], uid[1], uid[2], uid[3], uid[4], uid[5], uid[6]);
-    }
-
-    return true;
-}
+// Temporarily removed to avoid ff.h dependency
+// bool ntag_store_load_from_dump(const char* path) {
+//     FIL file;
+//     UINT bytes_read;
+//
+//     if (f_open(&file, path, FA_READ) != FR_OK) return false;
+//
+//     f_read(&file, current_tag.data, sizeof(current_tag.data), &bytes_read);
+//     f_close(&file);
+//
+//     if (bytes_read != sizeof(current_tag.data)) return false;
+//
+//     if (emu_callback) emu_callback(NTAG_EVENT_TYPE_UPDATE, emu_context);
+//
+//     if (emu_callback) {
+//         uint8_t* uid = &current_tag.data[0];
+//         NRF_LOG_INFO("Preserving UID from dump: %02X %02X %02X %02X %02X %02X %02X",
+//                      uid[0], uid[1], uid[2], uid[3], uid[4], uid[5], uid[6]);
+//     }
+//
+//     return true;
+// }
 
 int ntag_store_uuid_rand(ntag_t *ntag) {
     // Generate 7-byte random UID
