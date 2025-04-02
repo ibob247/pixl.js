a9279ab1587846395c92b482563661be070206a8
ibob247 <30962100+ibob247@users.noreply.github.com>
2025-04-02 19:58:48 -0400

    Update ntag_def.h

--- fw/application/src/ntag/ntag_def.h
+++ fw/application/src/ntag/ntag_def.h
@@ -2,28 +2,30 @@
 #define NTAG_DEF_H
 
 #include <stdint.h>
+#include <stdbool.h>
 
 #ifdef __cplusplus
 extern "C" {
 #endif
 
-#define NTAG_DATA_SIZE 540
+// Size of the NTAG memory (e.g., 540 bytes for NTAG215)
+#define NTAG_MEMORY_SIZE 540
 
-// Define the tag structure
-typedef struct {
-    uint8_t data[NTAG_DATA_SIZE];
-} ntag_t;
-
-// Define event types for tag updates
+// Event types for NTAG updates
 typedef enum {
     NTAG_EVENT_TYPE_READ,
     NTAG_EVENT_TYPE_UPDATE
 } ntag_event_type_t;
 
-// Callback type for tag update notifications
+// Structure representing an NTAG
+typedef struct {
+    uint8_t data[NTAG_MEMORY_SIZE];
+} ntag_t;
+
+// Callback type for NTAG updates
 typedef void (*ntag_update_cb_t)(ntag_event_type_t event, void* context, const ntag_t* tag);
 
-// Get the currently emulated tag
+// Gets the current tag being emulated
 const ntag_t* ntag_emu_get_current_tag(void);
 
 #ifdef __cplusplus
