55d14a5fcf9ccec941b48a5c593fb41f73fa799d
ibob247 <30962100+ibob247@users.noreply.github.com>
2025-04-02 18:05:08 -0400

    Update ntag_store.h

--- fw/application/src/ntag/ntag_store.h
+++ fw/application/src/ntag/ntag_store.h
@@ -1,28 +1,15 @@
-/*
- * ntag_store.h
- *
- *  Created on: 2021年9月1日
- *      Author: solos
- */
+#ifndef NTAG_STORE_H
+#define NTAG_STORE_H
 
-#ifndef NTAG_STORE_H_
-#define NTAG_STORE_H_
-
-#include "compiler_abstraction.h"
-#include "ntag_def.h"
-#include "sdk_errors.h"
-#include <stdbool.h>
 #include <stdint.h>
+#include <stdbool.h>
+#include "ntag.h"
+#include "nrf_error.h" // For ret_code_t
 
-// 填充默认ntag数据并随机uuid
-void ntag_store_new_rand(ntag_t *ntag);
-// 随机uuid数据(不修改其他)
+bool ntag_store_load_from_dump(const char* path);
 ret_code_t ntag_store_uuid_rand(ntag_t *ntag);
-void ntag_store_set_uuid(ntag_t *ntag, uint8_t *uuid);
+ret_code_t ntag_store_new_rand(ntag_t *ntag);
 void ntag_store_get_uuid(ntag_t *ntag, uint8_t *uuid);
+void ntag_store_set_uuid(ntag_t *ntag, uint8_t *uuid);
 
-ret_code_t ntag_store_read(uint8_t idx, ntag_t *ntag);
-ret_code_t ntag_store_write(uint8_t idx, ntag_t *ntag);
-ret_code_t ntag_store_reset(uint8_t idx, ntag_t *ntag);
-
-#endif /* NTAG_STORE_H_ */
+#endif // NTAG_STORE_H
