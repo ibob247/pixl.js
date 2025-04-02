1f7c586aa7d9db4fa59434b26d25b9a04999dd7f
ibob247 <30962100+ibob247@users.noreply.github.com>
2025-04-02 18:17:41 -0400

    Update ntag_store.h

--- fw/application/src/ntag/ntag_store.h
+++ fw/application/src/ntag/ntag_store.h
@@ -11,8 +11,8 @@
 #include "nrf_error.h"
 
 bool ntag_store_load_from_dump(const char* path);
-ret_code_t ntag_store_uuid_rand(ntag_t *ntag);
-ret_code_t ntag_store_new_rand(ntag_t *ntag);
+int ntag_store_uuid_rand(ntag_t *ntag);
+int ntag_store_new_rand(ntag_t *ntag);
 void ntag_store_get_uuid(ntag_t *ntag, uint8_t *uuid);
 void ntag_store_set_uuid(ntag_t *ntag, uint8_t *uuid);
 
