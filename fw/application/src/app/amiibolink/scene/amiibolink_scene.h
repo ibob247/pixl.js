0d7c4c2091bbb168a514dbe0e67ac07617bb43ba
ibob247 <30962100+ibob247@users.noreply.github.com>
2025-04-02 21:40:51 -0400

    Update amiibolink_scene.h

--- fw/application/src/app/amiibolink/scene/amiibolink_scene.h
+++ fw/application/src/app/amiibolink/scene/amiibolink_scene.h
@@ -1,14 +1,14 @@
+// amiibolink_scene.h
+
 #pragma once
 
-#include "mini_app.h"
-#include "amiibolink_view.h"
+#include "mini_app_registry.h"  // Required for mini_app_t
+#include "view.h"
 
-// Scene entry point functions
-void amiibolink_scene_init(mini_app_t* app);
-void amiibolink_scene_on_enter(void* context);
-void amiibolink_scene_on_exit(void* context);
-void amiibolink_scene_on_event(void* context, const mui_event_t* p_event);
+typedef struct {
+    mini_app_t* p_app;
+    view_t* p_view;
+    uint8_t amiibolink_mode;
+} amiibolink_scene_t;
 
-// Scene registration
-extern const mui_scene_t amiibolink_scene_main;
-
+void amiibolink_scene_register(void);
