0677c218a6f5b8db474926f9a3106127b1328864
ibob247 <30962100+ibob247@users.noreply.github.com>
2025-04-02 20:32:11 -0400

    Update mini_app_launcher.c

--- fw/application/src/core/mini_app_launcher.c
+++ fw/application/src/core/mini_app_launcher.c
@@ -1,24 +1,24 @@
 // mini_app_launcher.c
+
 #include "mini_app_launcher.h"
-#include "cache.h"
-#include "app_manager.h"
+#include "app_defines.h"
+#include "mod/cache.h"
+#include "mini_app_defines.h"
 #include "nrf_log.h"
 #include <string.h>
 
-void mini_app_launcher_run(mini_app_instance_t* app) {
+void mini_app_launcher_on_app_selected(const app_context_t* app, void** p_retain_data) {
+    cache_data_t* p_cache = cache_get_data();
+
     NRF_LOG_INFO("running APP: %d %s %d", app->p_app->id, nrf_log_push(app->p_app->name),
-                 app->p_app->requires_retain_data);
+                 (int)app->p_app->need_retain_data);
 
-    cache_data_t* p_cache = cache_get_data();  // ✅ Correct type now
-
-    if (app->p_app->requires_retain_data) {
+    if (app->p_app->need_retain_data) {
         p_cache->id = app->p_app->id;
         p_cache->enabled = true;
-        app->p_retain_data = p_cache->retain_data;
+        *p_retain_data = p_cache->retain_data;
     } else {
         p_cache->enabled = false;
         memset(p_cache->retain_data, 0, sizeof(p_cache->retain_data));
     }
-
-    app->p_app->entry(app);
 }
