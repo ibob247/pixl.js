e6ec03254d93ead9adf7ccf01a5abfb8824e55dd
ibob247 <30962100+ibob247@users.noreply.github.com>
2025-04-02 21:57:55 -0400

    Update amiibolink_scene.h

--- fw/application/src/app/amiibolink/scene/amiibolink_scene.h
+++ fw/application/src/app/amiibolink/scene/amiibolink_scene.h
@@ -1,14 +1,17 @@
-// amiibolink_scene.h
-
 #pragma once
 
-#include "mini_app_registry.h"
-#include "../../../view/view.h"  // <-- Corrected path to view.h
+#include "../../../view/view.h"
+#include "../../../core/mini_app.h"
 
 typedef struct {
-    mini_app_t* p_app;
     view_t* p_view;
-    uint8_t amiibolink_mode;
-} amiibolink_scene_t;
+    mini_app_t* p_app;
+    int amiibolink_mode;
+} amiibolink_app_t;
 
-void amiibolink_scene_register(void);
+void amiibolink_scene_main_on_enter(amiibolink_app_t* app);
+void amiibolink_scene_main_on_exit(amiibolink_app_t* app);
+void amiibolink_scene_main_on_event(amiibolink_app_t* app, view_event_t event);
+void amiibolink_scene_main_on_draw(amiibolink_app_t* app, canvas_t* p_canvas);
+
+#endif
