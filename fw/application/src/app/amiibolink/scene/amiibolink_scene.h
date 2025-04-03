95740b7d0cf78a896c6e4f5a89d41b5f5667e8be
ibob247 <30962100+ibob247@users.noreply.github.com>
2025-04-02 22:04:06 -0400

    Update amiibolink_scene.h

--- fw/application/src/app/amiibolink/scene/amiibolink_scene.h
+++ fw/application/src/app/amiibolink/scene/amiibolink_scene.h
@@ -1,15 +1,7 @@
 #pragma once
 
-#include "../view/view.h"
-#include "../../../core/mini_app.h"
+#include "../../view/view.h"
+#include "amiibolink_scene_main.h"
+#include "mini_app.h"
 
-typedef struct {
-    view_t* p_view;
-    mini_app_t* p_app;
-    int amiibolink_mode;
-} amiibolink_app_t;
-
-void amiibolink_scene_main_on_enter(amiibolink_app_t* app);
-void amiibolink_scene_main_on_exit(amiibolink_app_t* app);
-void amiibolink_scene_main_on_event(amiibolink_app_t* app, view_event_t event);
-void amiibolink_scene_main_on_draw(amiibolink_app_t* app, canvas_t* p_canvas);
+extern const view_t AMIIBOLINK_SCENE_MAIN;
