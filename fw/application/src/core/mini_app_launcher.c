e7a87e5851c1abc0a02b5c0babc081b6683a5fd3
ibob247 <30962100+ibob247@users.noreply.github.com>
2025-04-02 20:44:44 -0400

    Update mini_app_launcher.c

--- fw/application/src/core/mini_app_launcher.c
+++ fw/application/src/core/mini_app_launcher.c
@@ -1,30 +1,33 @@
 // mini_app_launcher.c
 #include "mini_app_launcher.h"
+#include "mini_app_runtime.h"
 #include "mini_app_defines.h"
 #include "cache.h"
-#include "nrf_log.h"
-#include "nrf_log_ctrl.h"
-#include "nrf_log_default_backends.h"
 #include <string.h>
+#include <stdbool.h>
+#include <nrf_log.h>
 
 void mini_app_launch(mini_app_runtime_t* app) {
+    NRF_LOG_INFO("running APP: %d %s %d",
+        app->p_app->id,
+        nrf_log_push(app->p_app->name),
+        app->p_app->run != NULL
+    );
+
     cache_data_t* p_cache = cache_get_data();
 
-    if (app->p_app) {
-        NRF_LOG_INFO("running APP: %d %s %d", app->p_app->id, nrf_log_push(app->p_app->name),
-                     app->p_app->retain_data_size);
+    if (app->p_app->run) {
+        p_cache->id = app->p_app->id;
+        p_cache->enabled = true;
+        app->p_retain_data = p_cache->retain_data;
 
-        if (p_cache) {
-            p_cache->id = app->p_app->id;
-            p_cache->enabled = true;
-            app->p_retain_data = p_cache->retain_data;
-        }
-    } else {
-        NRF_LOG_INFO("exiting APP");
-
-        if (p_cache) {
+        bool keep_running = app->p_app->run(app->p_retain_data);
+        if (!keep_running) {
             p_cache->enabled = false;
             memset(p_cache->retain_data, 0, sizeof(p_cache->retain_data));
         }
+    } else {
+        p_cache->enabled = false;
+        memset(p_cache->retain_data, 0, sizeof(p_cache->retain_data));
     }
 }
