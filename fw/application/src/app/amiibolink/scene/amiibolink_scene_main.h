228953373bc2a9d06356fb3316e97417750a2135
ibob247 <30962100+ibob247@users.noreply.github.com>
2025-04-02 21:28:30 -0400

    Update amiibolink_scene_main.h

--- fw/application/src/app/amiibolink/scene/amiibolink_scene_main.h
+++ fw/application/src/app/amiibolink/scene/amiibolink_scene_main.h
@@ -1,26 +1,9 @@
-#pragma once
+#ifndef AMIIBOLINK_SCENE_MAIN_H
+#define AMIIBOLINK_SCENE_MAIN_H
 
-#include "ble/ble_amiibolink_types.h"
-#include "ntag/ntag_def.h"
+#include "ble_amiibolink.h"
+#include "scene_manager.h"
 
-// Forward declaration of the app data structure
-typedef struct {
-    ble_amiibolink_mode_t amiibolink_mode;
-    bool show_animation;
-} amiibolink_scene_main_data_t;
+scene_t amiibolink_scene_main;
 
-// Callback when an NTAG event occurs
-void ntag_update_cb(ntag_event_type_t type, void* context, const ntag_t* p_ntag);
-
-// Entry point when the scene is entered
-void amiibolink_scene_main_on_enter(void* context);
-
-// Called periodically to update scene state
-bool amiibolink_scene_main_on_update(void* context);
-
-// Called when an input is received
-bool amiibolink_scene_main_on_event(void* context, const void* event);
-
-// Called before the scene is exited
-void amiibolink_scene_main_on_exit(void* context);
-
+#endif // AMIIBOLINK_SCENE_MAIN_H
